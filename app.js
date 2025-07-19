c7a0d703a7e25701214ff5d1f5bfbdd7a316cf01
khinpopohtut <khinpopohtut@gmail.com>
2025-07-19 10:52:00 +0630

    [ADD]19/7/25 ui frame setup

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require("express-session");
+const i18n = require("i18n");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+i18n.configure({
+  locales: ["en", "mm"],
+  directory: path.join(__dirname, "locales"),
+  defaultLocale: "mm",
+  queryParameter: "lang",
+  autoReload: true,
+  syncFiles: false,
+  objectNotation: true,
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +30,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+  session({
+    secret: "TUPathein@HC2025",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(i18n.init);
+
+app.use((req, res, next) => {
+  if (req.query.lang) {
+    req.setLocale(req.query.lang);
+    req.session.locale = req.query.lang;
+  } else if (req.session.locale) {
+    req.setLocale(req.session.locale);
+  }
+  res.locals.lang = req.getLocale();
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
