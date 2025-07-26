5b11ee9c9cf2775ebb647330784febe56019e12c
khinpopohtut <khinpopohtut@gmail.com>
2025-07-26 10:16:24 +0630

    [Add]26/7/25 HDL schema and init route

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var hiController = require("../controllers/hiControllers");
+var hdlController = require("../controllers/hdlController");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,5 +9,5 @@ router.get('/', function(req, res, next) {
 });
 
 router.use("/hi", hiController);
-
+router.use("/hdl", hdlController);
 module.exports = router;
