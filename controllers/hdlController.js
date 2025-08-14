9d48f806c9f29455cce6158befc118abba8b3ef5
khinpopohtut <khinpopohtut@gmail.com>
2025-08-14 16:46:18 +0630

    [Fix] image issue

--- controllers/hdlController.js
+++ controllers/hdlController.js
@@ -70,10 +70,13 @@ router.post("/edit", upload.single("image"), async function (req, res) {
       ageLevelEN: req.body.ageLevelEN,
       relatedIngredients: req.body.relatedIngredients,
     };
-    if (req.file) {
+    if (req.file) {update.image = "/images/uploads/" + req.file.filename;
       try {
-        if (hd.image) fs.unlinkSync("public" + hd.image);
-        update.image = "/images/uploads/" + req.file.filename;
+        if (hd.image){
+          await fs.access("public" + hd.image);
+          fs.unlinkSync("public" + hd.image);
+        } 
+        
       } catch (e) {
         console.log("Image error");
       }
